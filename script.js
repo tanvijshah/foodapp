539988a1edb82f2202a3d5e69008d82b4845f6d4
tanvijshah <32206820+tanvijshah@users.noreply.github.com>
2018-02-10 12:38:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-<script>
-document.getElementById("demo").innerHTML = "My First JavaScript";
-</script>
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
